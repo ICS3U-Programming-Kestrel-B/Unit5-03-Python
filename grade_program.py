4d1e568a18d48a354a2d84cfbc8490e1ac5291d8
Kestrel Bryce <Madeline.Bryce@stu.ocsb.ca>
2022-12-06 00:41:01 +0000

    Finished Python

--- grade_program.py
+++ grade_program.py
@@ -19,57 +19,66 @@ def main():
     # initializing user_mark
     user_mark = input("Enter a mark: ")
 
+    # making calculate function
     def calculate(user_mark):
         # initializing user_mark_percent
-        user_mark_percent = 0;
+        # user_mark_percent = 0;
         match user_mark:
             case "R":
-                user_mark_percent = 24.5;
-                return user_mark_percent;
+                user_mark_percent = 24.5
+                # return user_mark_percent;
             case "1-":
-                user_mark_percent = 51;
-                return user_mark_percent;
+                user_mark_percent = 51
+                # return user_mark_percent;
             case "1":
-                user_mark_percent = 54.5;
-                return user_mark_percent;
+                user_mark_percent = 54.5
+                # return user_mark_percent;
             case "1+":
-                user_mark_percent = 58;
-                return user_mark_percent;
+                user_mark_percent = 58
+                # return user_mark_percent;
             case "2-":
-                user_mark_percent = 61;
-                return user_mark_percent;
+                user_mark_percent = 61
+                # return user_mark_percent;
             case "2":
-                user_mark_percent = 64.5;
-                return user_mark_percent;
+                user_mark_percent = 64.5
+                # return user_mark_percent;
             case "2+":
-                user_mark_percent = 68;
-                return user_mark_percent;
+                user_mark_percent = 68
+                # return user_mark_percent;
             case "3-":
-                user_mark_percent = 71;
-                return user_mark_percent;
+                user_mark_percent = 71
+                # return user_mark_percent;
             case "3":
-                user_mark_percent = 74.5;
-                return user_mark_percent;
+                user_mark_percent = 74.5
+                # return user_mark_percent;
             case "3+":
-                user_mark_percent = 78;
-                return user_mark_percent;
+                user_mark_percent = 78
+                # return user_mark_percent;
             case "4-":
-                user_mark_percent = 83;
-                return user_mark_percent;
+                user_mark_percent = 83
+                # return user_mark_percent;
             case "4":
-                user_mark_percent = 90.5;
-                return user_mark_percent;
+                user_mark_percent = 90.5
+                # return user_mark_percent;
             case "4+":
-                user_mark_percent = 97.5;
-                return user_mark_percent;
+                user_mark_percent = 97.5
+                # return user_mark_percent;
             # invalid input
-            case "":
-                user_mark_percent = -10;
-                return user_mark_percent;
+            case _:
+                user_mark_percent = "invalid"
+        # return user_mark_percent;
         return user_mark_percent
 
-    # displaying to user
-    print(("The mark converted to a percent is {}%.").format(user_mark_percent))
+    # calling function
+    calculated_mark = calculate(user_mark)
+
+    # checking if input is valid
+    if calculated_mark != "invalid":
+        # displaying to user
+        print(("The mark converted to a percent is {}%.").format(calculated_mark))
+    else:
+        # error message
+        print("Please enter a valid mark.")
 
 
 if __name__ == "__main__":
